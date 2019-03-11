073c1d4460558625385824b9dd8f5317f1e3d378
Mitchell Hynes <mitchell.hynes@ecumene.xyz>
2019-03-11 00:12:56 -0230

    A rotating prism! But translation machine broke ;;(

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::screen::*;
+use rulinalg::vector::Vector;
+use rulinalg::matrix::Matrix;
+use std::io::{Write, stdout, stdin};
+use std::{str, f32};
+
+mod sloth;
+use sloth::Triangle;
+
+fn rot(x: f32, y: f32) -> Matrix<f32> {
+    Matrix::new(4, 4, vec![ x.cos(), 0.0, x.sin(), 0.0, 
+                            0.0,      y.cos(), -y.sin(),     0.0,
+                            -x.sin(), y.sin(), x.cos()*y.cos(), 0.0,
+                            0.0,      0.0, 0.0,     1.0])
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
+
+    let mut triangle = Triangle {
+        v1: Vector::new(vec![0.0, 0.0, 0.0, 1.0]),
+        v2: Vector::new(vec![-40.0, 40.0, 40.0, 1.0]),
+        v3: Vector::new(vec![40.0, 40.0, 40.0, 1.0])
+    };
+    let t = Matrix::new(4, 4, vec![1.0, 0.0, 0.0, 50.0, 
+                            0.0, 1.0, 0.0, 0.0,
+                            0.0, 0.0, 1.0, 0.0,
+                            0.0, 0.0, 0.0, 1.0]); // For whatever reason, transforming the rotation matrix with this breaks machine broke???
+    let mut x:f32 = 0.0;
+    let mut y:f32 = 0.0;
+    loop {
+        x += 0.03;
+        y += 0.01;
+        let size = termion::terminal_size().unwrap();
+        let mut frame_buffer = vec![0x0020u8; ((size.0-1)*(size.1-1)) as usize];
+        let mut z_buffer     = vec![f32::MAX; ((size.0-1)*(size.1-1)) as usize];
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, rot(x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, rot(std::f32::consts::PI/2.0 + x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, rot(std::f32::consts::PI + x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, rot(3.0*std::f32::consts::PI/2.0 + x, y), size.0 as usize, size.1 as usize);
+        match str::from_utf8(&frame_buffer) {
+            Ok(v) => println!("{}{}{}", termion::clear::All, termion::cursor::Goto(1,1), v),
+            Err(e) => panic!("Invalid UTF-8 shade chosen: {}", e),
+        };
+        stdout.flush().unwrap();
+
+        let c = stdin().keys().next().unwrap();
+        match c.unwrap() {
+            Key::Char('q') => break,
+            _=> {
+            }
+        }
+    }
+    stdout.flush().unwrap();
 }
--- src/sloth.rs
+++ src/sloth.rs
@@ -1,3 +1,8 @@
+use rulinalg::vector::Vector;
+use rulinalg::matrix::Matrix;
+
+const SHADES: [char; 6] = ['#', '*', '^', '\'', '`', ' '];
+
 // 2 3D points = Axis aligned bounding box
 pub struct AABB {
     min: Vector<f32>,
@@ -43,5 +48,55 @@ impl Triangle {
         self.v1 = &transform*&self.v1;
         self.v2 = &transform*&self.v2;
         self.v3 = &transform*&self.v3;
+    }
+}
+
+pub fn to_char(shade: f32, shades: [char; 6]) -> char {
+    if shade <= 0.20 {
+        shades[0]
+    } else if shade <= 0.40 {
+        shades[1]
+    } else if shade <= 0.60 {
+        shades[2]
+    } else if shade <= 0.80 {
+        shades[3]
+    } else if shade <= 1.0 {
+        shades[4]
+    } else {
+        shades[5]
+    }
+}
+
+// Used in rasterization
+fn orient(a: &Vector<f32>, b: &Vector<f32>, c: &Vector<f32>) -> f32 {
+    (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
+}
+
+pub fn draw_triangle(frame_buffer: &mut Vec<u8>, z_buffer: &mut Vec<f32>, triangle: &Triangle, transform: Matrix<f32>, width: usize, height: usize) {
+    let mut dist_triangle = triangle.clone();
+    dist_triangle.mul(transform);
+    // So we can just render what we need ( within bounds )
+    let aabb = dist_triangle.to_aabb();
+    let minx = aabb.min[0].max(0.0).floor() as usize;
+    let miny = aabb.min[1].max(0.0).floor() as usize;
+    let maxx = aabb.max[0].min((width-1) as f32).floor() as usize;
+    let maxy = aabb.max[1].min((height-1) as f32).floor() as usize;
+    let a = 1.0 / orient(&dist_triangle.v1, &dist_triangle.v2, &dist_triangle.v3);
+    for y in miny..maxy { // For Y in bounds
+        for x in minx..maxx { // For X in bounds
+            let p = Vector::new(vec![x as f32, y as f32]);
+            let w0 = orient(&dist_triangle.v2, &dist_triangle.v3, &p);
+            let w1 = orient(&dist_triangle.v3, &dist_triangle.v1, &p);
+            let w2 = orient(&dist_triangle.v1, &dist_triangle.v2, &p);
+            if w0>=0.0 && w1>=0.0 && w2 >=0.0 { // Does it past the test?
+                let z = &dist_triangle.v1[2] + a*(w1*(&dist_triangle.v2[2] - &dist_triangle.v1[2]) + w2*(&dist_triangle.v3[2] - &dist_triangle.v1[2]));
+                let shade = z/10.0;
+                let id = y*width + x;
+                if z < z_buffer[id] {
+                    z_buffer[id] = z;
+                    frame_buffer[id] = to_char(shade, SHADES) as u8; // Sample the bytes -> Sample the shades with 10 thresholds
+                }
+            }
+        }
     }
 }
