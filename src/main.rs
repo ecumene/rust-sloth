a78da728c6e14a89a421e322a4368bda2451eb02
Mitchell Hynes <mitchell.hynes@ecumene.xyz>
2019-03-16 20:00:50 -0230

    Perspective test

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use termion::screen::*;
 use std::io::{Write, stdout, stdin};
 use std::{str, f32};
 
-use nalgebra::{Matrix4, Vector4};
+use nalgebra::{Matrix4, Vector4, Perspective3};
 
 mod sloth;
 use sloth::Triangle;
@@ -22,23 +22,25 @@ fn main() {
 
     let mut triangle = Triangle {
         v1: Vector4::new(0.0, 0.0, 0.0, 1.0),
-        v2: Vector4::new(-30.0, 30.0, 30.0, 1.0),
-        v3: Vector4::new(30.0, 30.0, 30.0, 1.0)
+        v2: Vector4::new(-10.0, 10.0, 10.0, 1.0),
+        v3: Vector4::new(10.0, 10.0, 10.0, 1.0)
     };
-    let t = Matrix4::new(1.0, 0.0, 0.0, 30.0, 
+    let persp = Perspective3::new(1.0, 3.14 / 4.0, 1.0, 10.0);
+    let persp = persp.as_matrix();
+    let t = Matrix4::new(1.0, 0.0, 0.0, 10.0, 
                          0.0, 1.0, 0.0, 0.0,
                          0.0, 0.0, 1.0, 0.0,
-                         0.0, 0.0, 0.0, 1.0); // For whatever reason, transforming the rotation Matrix4 with this breaks machine broke???
+                         0.0, 0.0, 0.0, 1.0);
     let mut x:f32 = 0.0;
     let mut y:f32 = 0.0;
     loop {
         let size = termion::terminal_size().unwrap();
         let mut frame_buffer = vec![0x0020u8; ((size.0-1)*(size.1-1)) as usize];
         let mut z_buffer     = vec![f32::MAX; ((size.0-1)*(size.1-1)) as usize];
-        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, &t*rot(x, y), size.0 as usize, size.1 as usize);
-        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, &t*rot(std::f32::consts::PI/2.0 + x, y), size.0 as usize, size.1 as usize);
-        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, &t*rot(std::f32::consts::PI + x, y), size.0 as usize, size.1 as usize);
-        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, &t*rot(3.0*std::f32::consts::PI/2.0 + x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, persp*&t*rot(x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, persp*&t*rot(std::f32::consts::PI/2.0 + x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, persp*&t*rot(std::f32::consts::PI + x, y), size.0 as usize, size.1 as usize);
+        sloth::draw_triangle(&mut frame_buffer, &mut z_buffer, &triangle, persp*&t*rot(3.0*std::f32::consts::PI/2.0 + x, y), size.0 as usize, size.1 as usize);
         match str::from_utf8(&frame_buffer) {
             Ok(v) => println!("{}{}{}", termion::clear::All, termion::cursor::Goto(1,1), v),
             Err(e) => panic!("Invalid UTF-8 shade chosen: {}", e),
