946f945203236969c9a201d82e9350515d718d05
mitch <mitchell.hynes@ecumene.xyz>
2019-03-27 10:39:38 -0230

    Added a tobj to simple mesh function

--- src/base/geometry.rs
+++ src/base/geometry.rs
@@ -1,4 +1,6 @@
 use nalgebra::{Matrix4, Vector4, Unit};
+use std::clone::Clone;
+use tobj::{Mesh};
 
 // 2 3D points = Axis aligned bounding box
 pub struct AABB {
@@ -32,14 +34,6 @@ impl Triangle {
             Vector4::from_fn(|x, _size| self.v1[x].max(self.v2[x].max(self.v3[x])))
         )   
     }
-    // This clones the triangle (Its points)
-    pub fn clone(&self) -> Triangle {
-        Triangle {
-            v1: self.v1.clone(),
-            v2: self.v2.clone(),
-            v3: self.v3.clone()
-        }
-    }
     // This mutates the triangle's points into a given Matrix space
     pub fn mul(&mut self, transform: Matrix4<f32>) {
         self.v1 = &transform*&self.v1;
@@ -56,6 +50,42 @@ impl Triangle {
     }
 }
 
-pub struct Mesh {
+impl Clone for Triangle {
+    fn clone(&self) -> Triangle {
+        Triangle {
+            v1: self.v1.clone(),
+            v2: self.v2.clone(),
+            v3: self.v3.clone()
+        }
+    }
+}
+
+pub trait ToSimpleMesh {
+    fn to_simple_mesh(&self) -> SimpleMesh;
+}
+
+pub struct SimpleMesh {
     pub triangles: Vec<Triangle>
+}
+
+impl ToSimpleMesh for Mesh {
+    fn to_simple_mesh(&self) -> SimpleMesh {
+        let mut triangles = vec![Triangle {
+            v1: Vector4::new(0.0,0.0,0.0,1.0), 
+            v2: Vector4::new(0.0,0.0,0.0,1.0),
+            v3: Vector4::new(0.0,0.0,0.0,1.0)
+        }; self.indices.len()/3];
+        for x in 0..triangles.len() {
+            triangles[x].v1.x = self.positions[(self.indices[x*3]*3) as usize];
+            triangles[x].v1.y = self.positions[(self.indices[x*3]*3 + 1) as usize];
+            triangles[x].v1.z = self.positions[(self.indices[x*3]*3 + 2) as usize];
+            triangles[x].v2.x = self.positions[(self.indices[x*3 + 1]*3) as usize];
+            triangles[x].v2.y = self.positions[(self.indices[x*3 + 1]*3 + 1) as usize];
+            triangles[x].v2.z = self.positions[(self.indices[x*3 + 1]*3 + 2) as usize];
+            triangles[x].v3.x = self.positions[(self.indices[x*3 + 2]*3) as usize];
+            triangles[x].v3.y = self.positions[(self.indices[x*3 + 2]*3 + 1) as usize];
+            triangles[x].v3.z = self.positions[(self.indices[x*3 + 2]*3 + 2) as usize];
+        }
+        SimpleMesh { triangles: triangles }
+    }
 }
