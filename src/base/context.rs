72a9a5ee0f6ff5753418b3dfac0e34cf1463f81f
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2019-03-31 22:08:43 -0600

    fix some minor problems; run cargo fmt

--- src/base/context.rs
+++ src/base/context.rs
@@ -1,5 +1,5 @@
-use std::{str, f32};
-use nalgebra::{Matrix4};
+use nalgebra::Matrix4;
+use std::{f32, str};
 
 pub struct Context {
     pub utransform: Matrix4<f32>,
@@ -10,29 +10,34 @@ pub struct Context {
 }
 
 impl Context {
-    pub fn blank () -> Context { //TODO: Make this a constant struct
+    pub fn blank() -> Context {
+        //TODO: Make this a constant struct
         Context {
-            utransform: Matrix4::new(1.0, 0.0, 0.0, 0.0,
-                                     0.0, 1.0, 0.0, 0.0,
-                                     0.0, 0.0, 1.0, 0.0,
-                                     0.0, 0.0, 0.0, 1.0),
+            utransform: Matrix4::new(
+                1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+            ),
             width: 0,
             height: 0,
             frame_buffer: vec![],
-            z_buffer: vec![]
+            z_buffer: vec![],
         }
     }
-    pub fn clear (&mut self) {
-        self.frame_buffer = vec![0x0020u8; self.width*self.height as usize]; //0x0020u8 is the default space character
-        self.z_buffer     = vec![f32::MAX; self.width*self.height as usize]; //f32::MAX is written to the z-buffer as an infinite back-wall to render with
+    pub fn clear(&mut self) {
+        self.frame_buffer = vec![0x0020u8; self.width * self.height as usize]; //0x0020u8 is the default space character
+        self.z_buffer = vec![f32::MAX; self.width * self.height as usize]; //f32::MAX is written to the z-buffer as an infinite back-wall to render with
     }
-    pub fn camera (&mut self, proj: Matrix4<f32>, view: Matrix4<f32>) -> &Matrix4<f32> {
-        self.utransform = proj*view;
+    pub fn camera(&mut self, proj: Matrix4<f32>, view: Matrix4<f32>) -> &Matrix4<f32> {
+        self.utransform = proj * view;
         &self.utransform
     }
-    pub fn flush (&self) {
+    pub fn flush(&self) {
         match str::from_utf8(&self.frame_buffer) {
-            Ok(v) => println!("{}{}{}", termion::clear::All, termion::cursor::Goto(1,1), v), // TODO: Create colored materials via 24-bit RGB colors
+            Ok(v) => println!(
+                "{}{}{}",
+                termion::clear::All,
+                termion::cursor::Goto(1, 1),
+                v
+            ), // TODO: Create colored materials via 24-bit RGB colors
             Err(e) => panic!("Invalid UTF-8 shade chosen: {}", e),
         };
     }
--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,7 +1,7 @@
-pub mod geometry;
 pub mod context;
+pub mod geometry;
 pub mod rasterizer;
 
-pub use self::geometry::*;
 pub use self::context::*;
-pub use self::rasterizer::*; 
+pub use self::geometry::*;
+pub use self::rasterizer::*;
