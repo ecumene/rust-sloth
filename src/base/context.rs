a7b7c608c497c1503e3e0e79e2dc1dd0d0c40104
Mitchell Hynes <mitchell.hynes@ecumene.xyz>
2019-04-19 21:24:17 -0230

    Pulled things out of main.rs, added shaders

--- src/base/context.rs
+++ src/base/context.rs
@@ -1,4 +1,5 @@
 use nalgebra::Matrix4;
+use crate::base::SimpleMesh;
 use std::{f32};
 
 pub struct Context {
@@ -22,7 +23,7 @@ impl Context {
             width: 0,
             height: 0,
             frame_buffer: vec![],
-            z_buffer: vec![],
+            z_buffer: vec![]
         }
     }
     pub fn clear(&mut self) {
@@ -39,5 +40,31 @@ impl Context {
             x.push_str(pixel);
         }
         println!("{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), x);
+    }
+    pub fn update(&mut self, mut old_size: (u16, u16),
+        meshes: &[SimpleMesh],
+        ) {
+        let terminal_size = termion::terminal_size().unwrap(); // Temporary size
+        if old_size != terminal_size {
+            // Check if the size changed
+            old_size = terminal_size; // It changed! Set new size
+            let mut scale: f32 = 0.0; // The scene's scale
+            for mesh in meshes {
+                // This calculates the maximum axis value (x y or z) in all meshes
+                scale = scale
+                    .max(mesh.bounding_box.max.x)
+                    .max(mesh.bounding_box.max.y)
+                    .max(mesh.bounding_box.max.z);
+            }
+            scale = f32::from(old_size.1).min(f32::from(old_size.0) / 2.0) / scale / 2.0; // Constrain to width and height, whichever is smaller
+            let t = Matrix4::new(scale,    0.0,   0.0, f32::from(old_size.0) / 4.0, // X translation is divided by 4 because there's a 1 char space between charxels
+                                0.0, -scale,   0.0, f32::from(old_size.1) / 2.0, // Y translation is divided by 2 to center
+                                0.0,    0.0, scale,                         0.0,
+                                0.0,    0.0,   0.0,                         1.0,
+            );
+            self.utransform = t;
+            self.width = (old_size.0) as usize;
+            self.height = (old_size.1 - 1) as usize;
+        }
     }
 }
